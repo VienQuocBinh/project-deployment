14af658cafedf78565cab80dbccfb659763d6aea
VienQuocBinh <quocbinhvien@gmail.com>
2025-10-01 02:46:15 +0700

    Get user by Id

--- src/main/java/vieb/sideproject/controller/UserController.java
+++ src/main/java/vieb/sideproject/controller/UserController.java
@@ -2,6 +2,7 @@ package vieb.sideproject.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import vieb.sideproject.entity.User;
@@ -14,6 +15,15 @@ public class UserController {
         return ResponseEntity.ok(User.builder()
                 .id(1L)
                 .username("user1")
+                .password("pwd")
+                .build());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") String id) {
+        return ResponseEntity.ok(User.builder()
+                .id(Long.valueOf(id))
+                .username("user " + id)
                 .password("pwd")
                 .build());
     }
